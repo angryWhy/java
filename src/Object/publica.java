652ce699a5ae55d78d474778f82c71b95b5ebcc9
angryWhy <875978822@qq.com>
2022-10-26 21:55:02 +0800

    control

--- src/Object/publica.java
+++ src/Object/publica.java
@@ -2,8 +2,8 @@ package Object;
 
 public class publica {
     //访问修饰符
-    //public，对外公开
-    //protected，对子类和本包中的类公开
-    //默认，同一个包的类公开
+    //public，对外公开，同类，子类，不同包，本包
+    //protected，对子类和本包，本类
+    //默认，同一个包的类公开,本类
     //私有，private只有本身的类可以访问，不对外公开
 }
