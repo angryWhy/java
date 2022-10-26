220e656bba1649b28720c2d97868bacec0d04b4b
angryWhy <875978822@qq.com>
2022-10-26 21:50:40 +0800

    pkg

---
+++ src/Object/publica.java
@@ -0,0 +1,9 @@
+package Object;
+
+public class publica {
+    //访问修饰符
+    //public，对外公开
+    //protected，对子类和本包中的类公开
+    //默认，同一个包的类公开
+    //私有，private只有本身的类可以访问，不对外公开
+}
---
+++ src/pkg/basic.java
@@ -0,0 +1,17 @@
+package pkg;
+
+import java.util.Scanner;
+
+public class basic {
+    //区分相同名字的类
+    //类很多的时候，可以很好地管理类
+    //控制访问范围
+
+
+    //命名规范
+    //com.公司名.项目名.业务模块名
+    //com.sina.crm.user
+    public static void main(String[] args) {
+
+    }
+}
