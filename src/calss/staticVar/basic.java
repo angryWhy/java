39ed68b4a79569537701151acf81fe75295a38fc
angryWhy <875978822@qq.com>
2022-10-31 21:24:31 +0800

    static

---
+++ src/calss/staticVar/basic.java
@@ -0,0 +1,16 @@
+package calss.staticVar;
+
+public class basic {
+    //定义一个静态变量，static 静态
+    //改变量最大的特点就是会被child类所有对象的实例共享
+    public static int count = 0;
+
+    public static void main(String[] args) {
+        basic a = new basic();
+        System.out.println(a.count);
+    }
+    //静态方法，访问静态变量
+    public static void say(){
+        System.out.println(count);
+    }
+}
