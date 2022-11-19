ac5126e0065e5f247d1f88321512f845a7539d49
angryWhy <875978822@qq.com>
2022-11-19 15:08:59 +0800

    KeyListener

--- src/BallMove/Ball.java
+++ src/BallMove/Ball.java
@@ -4,8 +4,10 @@ package BallMove;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Ball extends JFrame{
+public class Ball extends JFrame {
     private MyPanel mp = null;
 
     public Ball()  {
@@ -14,15 +16,34 @@ public class Ball extends JFrame{
         this.setSize(300,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        this.addKeyListener(mp);
     }
     public static void main(String[] args) {
         new Ball();
     }
 }
-class MyPanel extends JPanel {
+//监听键盘发生的事件
+class MyPanel extends JPanel implements KeyListener {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        g.fillOval(10,10,20,20);
+        g.fillOval(10, 10, 20, 20);
+    }
+
+    //有字符输出，该方法会触发
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    //某个键按下，触发
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println((char)e.getKeyCode()+"按下了");
+    }
+    //某个键释放了，触发 
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
