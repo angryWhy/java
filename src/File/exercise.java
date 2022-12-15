bd71e4e483c7f01c62a1d5ce9eae0a42ce623c85
angryWhy <875978822@qq.com>
2022-12-15 20:45:32 +0800

    IO

--- src/File/exercise.java
+++ src/File/exercise.java
@@ -1,6 +1,7 @@
 package File;
 
 import java.io.*;
+import java.util.Properties;
 
 public class exercise {
     public static void main(String[] args) {
@@ -42,5 +43,47 @@ public class exercise {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    public void exercise03(){
+        String filePath = "d:\\a.txt";
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(filePath));
+            properties.list(System.out);
+            Object name = properties.getProperty("name");
+            Object age = Integer.parseInt( properties.getProperty("age"));
+            Dog dog = new Dog(1,2);
+            String file = "d:\\data.dat";
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file));
+            objectOutputStream.writeObject(dog);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void sy(){
+        String filePath = "d:\\a.txt";
+        try {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(filePath));
+            try {
+                Object o = objectInputStream.readObject();
+                Dog dog = (Dog)o;
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+class Dog{
+    private int name;
+    private int age;
+    public void say(){
+
+    }
+    public Dog(int name,int age){
+        this.name = name;
+        this.age = age;
     }
 }
